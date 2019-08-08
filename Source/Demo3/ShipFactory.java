02513b8f89d983489c69620b2ba79aa9ebdaaa6d
BPhan1110 <52811199+BPhan1110@users.noreply.github.com>
2019-08-08 12:50:52 -0600

    Update ShipFactory.java

--- Source/Demo3/ShipFactory.java
+++ Source/Demo3/ShipFactory.java
@@ -14,12 +14,24 @@ import battleship.controllers.ToolsClassController;
 public class ShipFactory {
 	private static ShipFactory factory = new ShipFactory();
 	
+	/**
+	 * Default constructor for the ShipFactory class.
+	 */
 	private ShipFactory(){}
 	
+	/**
+	 * Get the factory instance in the shipFactory class.
+	 * @return factory
+	 */
 	public static ShipFactory getFactory() {
 		return factory;
 	}
 	
+	/**
+	 * Stores ship data from the Ship Class and returns a list of type Ship[]
+	 * @param numberOfShips
+	 * @return ships
+	 */
 	public Ship[] getShipsByNumber(Integer numberOfShips){
 		Ship[] ships = new Ship[numberOfShips];
 		for(int i = 0;i<=ships.length-1;i++) {
