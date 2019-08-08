c9f69fbd3e9007df934e9fe8040a77585aa1771a
BPhan1110 <52811199+BPhan1110@users.noreply.github.com>
2019-08-08 12:40:11 -0600

    Update GameoverController.java

--- Source/Demo3/GameoverController.java
+++ Source/Demo3/GameoverController.java
@@ -5,24 +5,41 @@ package battleship.controllers;
  */
 import java.util.ArrayList;
 import java.util.List;
-
 import battleship.ship.Ship;
 
 public class GameoverController{
 	// Create a list to store the remaining ships on the game board.
 	List<Ship> shipsRemaining = new ArrayList<>();
 	
+	/**
+	 * Default constructor.
+	 */
+	GameoverController() {}
+	
 	//Getter and setter methods for this list.
+	
+	/**
+	 * Get the list of ships that remain on the board
+	 * @return shipsRemaining
+	 */
 	List<Ship> getShipsRemaining() {
 		return shipsRemaining;
 	}
-
+	
+	/**
+	 * Set the remaining ships on the board into a list
+	 * @param remaining 
+	 */
 	void setShipsRemaining(List<Ship> remaining) {
 		shipsRemaining = remaining;
 	}
 	
 
-	//To judge how many ships are still on the game board. When all ships have been damaged, the game will be closed.
+	/**
+	 * To judge how many ships are still on the game board. When all ships have been damaged 
+	 * and the shipsRemaining list is empty (all ships have 0 health), the game will terminate.
+	 * @param ship 
+	 */
 	public void judgeGameover(Ship ship) {
 		if(ship.getShipHealth()==0) {
 				shipsRemaining.remove(ship);
@@ -33,7 +50,4 @@ public class GameoverController{
 			System.exit(0);
 		}
 	}
-	
-	// Default constructor.
-	GameoverController() {}
 }
